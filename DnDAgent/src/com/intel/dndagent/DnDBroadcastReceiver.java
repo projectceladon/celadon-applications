864ade435c05e32a5a1adb55ac68cb08549fff47
Ai, Ting <ting.a.ai@intel.com>
2021-12-23 17:27:30 +0800

    Add drag and drop agent
    Signed-off-by: Ai, Ting <ting.a.ai@intel.com>

---
+++ DnDAgent/src/com/intel/dndagent/DnDBroadcastReceiver.java
@@ -0,0 +1,115 @@
+package com.intel.dndagent;
+
+import java.io.File;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
+
+public class DnDBroadcastReceiver extends BroadcastReceiver{
+    private final static String TAG = "DnDBroadcastReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String data=intent.getExtras().get("data").toString();
+
+        sendFiletoWX(context,data);
+    }
+
+    private void sendFiletoWX(Context context,String data) {
+        String filePath = Environment.getExternalStorageDirectory() + "/Download/DnDData/" + data;
+        File file = new File(filePath);
+
+        Uri contentPath = FileProvider.getUriForFile(context,
+                context.getApplicationContext().getPackageName() + ".fileprovider", file);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        ComponentName comp = new ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareImgUI");
+        intent.setComponent(comp);
+        intent.setPackage(context.getPackageName());
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+	String type = "";
+        for (int i = 0; i < TYPE_ARRAY.length; i++) {
+            if (file.getAbsolutePath().toString().contains(TYPE_ARRAY[i][0].toString())) {
+                type = TYPE_ARRAY[i][1];
+                break;
+            }
+        }
+        intent.setType(type);
+        intent.putExtra(Intent.EXTRA_STREAM, contentPath);
+        context.startActivity(intent);
+    }
+
+    private static final String[][] TYPE_ARRAY = {
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".prop", "text/plain"},
+            {".rar", "application/x-rar-compressed"},
+            {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"},
+            {".rtf", "application/rtf"},
+            {".sh", "text/plain"},
+            {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"},
+            {".txt", "text/plain"},
+            {".wav", "audio/x-wav"},
+            {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"},
+            {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/zip"},
+            {"", "*/*"}
+    };
+}
